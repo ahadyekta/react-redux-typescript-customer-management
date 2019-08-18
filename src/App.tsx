9bf9132e76bb7023490336eaeb67c321cd34a342
ahad nemati <ahad.nemati@myer.com.au>
2019-08-18 14:11:09 +1000

    config for deploying to gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Route, HashRouter } from 'react-router-dom'
 import List from './components/pages/List/.'
 import Form from './components/pages/Form/.'
 import { CREATE_URL, LIST_URL, EDIT_URL } from './constansts'
 
 const App: React.FC = () => {
     return (
-        <Switch>
+        <HashRouter basename="/">
             <Route path={LIST_URL} exact component={List} />
             <Route path={CREATE_URL} exact component={Form} />
             <Route path={`${EDIT_URL}/:id`} exact component={Form} />
-        </Switch>
+        </HashRouter>
     )
 }
 
